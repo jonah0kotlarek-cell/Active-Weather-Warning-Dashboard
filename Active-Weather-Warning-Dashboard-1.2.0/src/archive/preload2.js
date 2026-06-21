cec0b0a4588ff01f60720d76cf59e4f1aad45664
jonah0kotlarek-cell <jonah0kotlarek@gmail.com>
2026-06-21 18:57:37 -0400

    remove old 1.2.0 archive folder

--- Active-Weather-Warning-Dashboard-1.2.0/src/archive/preload2.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  minimize: () => ipcRenderer.send('win-minimize'),
-  close:    () => ipcRenderer.send('win-close'),
-  setAlwaysOnTop: (val) => ipcRenderer.send('win-toggle-ontop', val),
-});
